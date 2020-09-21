9b09b63d5d9837797222bb196ce8082be9394292
Wendy Plas <w.j.t.plas@hotmail.com>
2020-09-21 13:12:47 +0200

    Initial commit

---
+++ build/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="author" content="" />
+        <meta name="description" property="og:description" content="" />
+        <meta name="image" property="og:image" content="">
+        <meta name="publish_date" property="og:publish_date" content="2020-16-09T12:15:00-0600">
+        <meta name="keywords"  content="" />
+        <title></title>
+        <link rel="shortcut icon" type="image/jpg" href="images/Logo.png"/> <!-- favicon -->
+        <link rel="stylesheet" type="text/css" href="css/index.css"/> <!-- stylesheet -->  
+    </head> 
+    <body>
+        <?php $page = 'index'; require 'Navigatie.php';?>  
+         <section class="col-12 grid"> 
+            <div class="col-2" id=""></div>
+            <div class="col-8" id="titel">
+                <h1> Explore how technology can be used to restore balance when it comes to our social needs</h1>
+            </div> 
+            <div class="col-2" id=""></div>
+
+            <div class="col-6" id="space">
+                <p class="left">We all have a need to balance our time between being together and being alone. For each and every single person the ideal balance is different. Technology (in all its complexity) influences our ability to keep this balance. What happens if we make our own social technologies? What do we learn about our social needs and our use of technology?</p>
+                <img src="images/GPL.png" alt="GPL" width="104" >
+            </div>
+            <div class="col-6" id="space">
+                <img src="images/Arduino.png" alt="Arduino" width="564">
+            </div>
+
+            <div class="col-6" id="space" >
+                <img src="images/Moti.jpg" alt="Moti" width="390" class="imgcenter">
+                <h1 class="imgcenter">What others made</h1>
+                <p class="imgcenter">Read about what others made, how they lived with it and what it learned them about themselves, technology and their social needs.</p>
+                <button type="button"><a href="https://medium.com/connectek">PROJECT STORIES</a></button>
+            </div>
+
+            <div class="col-6" id="space">
+                <img src="images/Logo.png" alt="Logo" width="390" class="imgcenter">
+                <h1 class="imgcenter">Make your own</h1>
+                <p class="imgcenter">Would you like to make your own technology and see what it can mean for your social needs? Join the exploration! Visit our making guide and join the community forum.</p>
+                <button type="button"><a href="https://pamelaanne.github.io/SocialRobot/">I WANT TO MAKE!</a></button>
+                <button type="button"><a href="https://whiterose.tribe.so/">OUR COMMUNITY</a></button>
+            </div>
+
+            <div class="col-6" id="space">
+                <img src="images/GroupPicture.jpeg" alt="GroupPicture" width="564">
+            </div>
+            <div class="col-6" id="space">
+                <h1>About Us</h1>
+                <p class="left">This project started as a research project of four Master Digital Design students. When students Pamela, Wendy, Veronika and Jasper found themselves stuck at home due to Covid-19, they started experimenting with social technologies. This experience was so valuable to them that they developed this platform to make it available to more people. </p>
+            </div>
+
+        </section>
+        <?php require 'Footer.php';?> 
+        <script src="js/bundle.js"></script>         
+    </body>
+</html>
